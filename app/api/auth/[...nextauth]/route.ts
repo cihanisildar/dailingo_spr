009997c343d36ddb6e0cb1be98b79351beff6c83
Cihan <officialcihan0248@gmail.com>
2025-05-23 17:16:05 +0300

    next auth bug fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST }; 
+// Create and export handler functions directly
+export const GET = NextAuth(authOptions);
+export const POST = NextAuth(authOptions); 
